c9f2735e6dca01c41dc1cc3661d0f17258ad6c34
Wes Cook <im@wescook.ca>
2016-09-21 19:35:56 -0700

    Storing fluid type as NBT data.  Adding particle effects during watering/refilling.  Watering can now moistens farmland.

--- src/main/java/ca/wescook/wateringcans/items/ItemWateringCan.java
+++ src/main/java/ca/wescook/wateringcans/items/ItemWateringCan.java
@@ -2,18 +2,20 @@ package ca.wescook.wateringcans.items;
 
 import ca.wescook.wateringcans.WateringCans;
 import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -21,6 +23,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static java.util.Arrays.asList;
+import static net.minecraft.block.BlockFarmland.MOISTURE;
 
 class ItemWateringCan extends Item {
 	ItemWateringCan() {
@@ -28,6 +31,7 @@ class ItemWateringCan extends Item {
 		setUnlocalizedName(WateringCans.MODID + ".watering_can");
 		GameRegistry.register(this);
 		setCreativeTab(CreativeTabs.TOOLS);
+		setMaxStackSize(1);
 		renderModel();
 	}
 
@@ -40,43 +44,70 @@ class ItemWateringCan extends Item {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
 	{
+		// List of valid fluid blocks
+		String[] validBlocks = new String[]{"water", WateringCans.MODID + ":growth_solution_block"};
+
 		// Ray trace - find block we're looking at
-		RayTraceResult raytraceresult = this.rayTrace(worldIn, playerIn, true);
+		RayTraceResult rayTraceResult = this.rayTrace(worldIn, playerIn, true);
 
 		// If sky, ignore
-		if (raytraceresult == null) {
+		if (rayTraceResult == null) {
 			return new ActionResult(EnumActionResult.PASS, itemStackIn);
 		}
 		else {
 			// If a block is found (includes fluids)
-			if (raytraceresult.typeOfHit == RayTraceResult.Type.BLOCK) {
+			if (rayTraceResult.typeOfHit == RayTraceResult.Type.BLOCK) {
+
+				// Get exact vector to later spawn particles there
+				Vec3d rayTraceVector = rayTraceResult.hitVec;
 
 				// Collect information about block
-				BlockPos blockpos = raytraceresult.getBlockPos(); // Get block position from ray trace
-				Block selectedBlock = worldIn.getBlockState(blockpos).getBlock(); // Get block object
-				String blockNameRaw = selectedBlock.getUnlocalizedName(); // Get block name
+				BlockPos blockPos = rayTraceResult.getBlockPos(); // Get block position from ray trace
+				Block blockObj = worldIn.getBlockState(blockPos).getBlock(); // Get block object
+				String blockNameRaw = blockObj.getUnlocalizedName(); // Get block name
 				String blockName = blockNameRaw.substring(5); // Clean .tile prefix
 
-				// Make a list of valid blocks
-				String[] validBlocks = new String[]{"water", WateringCans.MODID + ":growth_solution_block"};
-
-				// If found block is in list
-				if (asList(validBlocks).contains(blockName)) {
-					worldIn.playSound(playerIn, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.NEUTRAL, 1.0F, 1.0F); // Play sound
-					worldIn.destroyBlock(blockpos, false); // Destroy block
-
-					// If water
-					if (blockName.equals("water")) {
-						System.out.println("Water");
-					}
-					// If growth solution
-					else if (blockName.equals(WateringCans.MODID + ":growth_solution_block")) {
-						System.out.println("Growth solution");
-					}
-				}
+				// If found block is in fluid list, refill watering can
+				if (asList(validBlocks).contains(blockName))
+					refillWateringCan(worldIn, playerIn, itemStackIn, blockName, blockPos);
+				else // Water that block
+					commenceWatering(worldIn, rayTraceVector, blockName, blockPos, blockObj);
 			}
 
 			return new ActionResult(EnumActionResult.PASS, itemStackIn);
 		}
+	}
+
+	private void refillWateringCan(World worldIn, EntityPlayer playerIn, ItemStack itemStackIn, String blockName, BlockPos blockPos) {
+		worldIn.playSound(playerIn, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.NEUTRAL, 1.0F, 1.0F); // Play sound
+		worldIn.destroyBlock(blockPos, false); // Destroy block
+
+		// Create bubbles
+		for (int i=0; i<10; i++)
+			worldIn.spawnParticle(EnumParticleTypes.WATER_BUBBLE, blockPos.getX() + 0.5 + (worldIn.rand.nextGaussian() * 0.3D), blockPos.getY() + 1, blockPos.getZ() + 0.5 + (worldIn.rand.nextGaussian() * 0.3D), 0.0D, 0.0D, 0.0D);
+
+		// Check for/create NBT tag
+		NBTTagCompound nbtCompound = itemStackIn.getTagCompound(); // Check if exists
+		if (nbtCompound == null) // If not
+			nbtCompound = new NBTTagCompound(); // Create new compound
+
+		// Assign fluid based on block
+		if (blockName.equals("water"))
+			nbtCompound.setString("fluid", "water");
+		else if (blockName.equals(WateringCans.MODID + ":growth_solution_block"))
+			nbtCompound.setString("fluid", "growth_solution");
+
+		nbtCompound.setByte("amount", (byte) 127); // Refill watering can
+		itemStackIn.setTagCompound(nbtCompound); // Attach to itemstack
+	}
+
+	private void commenceWatering(World worldIn, Vec3d rayTraceVector, String blockName, BlockPos blockPos, Block blockObj) {
+		// Create water particles
+		for (int i=0; i<25; i++)
+			worldIn.spawnParticle(EnumParticleTypes.WATER_SPLASH, rayTraceVector.xCoord + (worldIn.rand.nextGaussian() * 0.18D), rayTraceVector.yCoord, rayTraceVector.zCoord + (worldIn.rand.nextGaussian() * 0.18D), 0.0D, 0.0D, 0.0D);
+
+		// Moisten soil
+		if (blockName.equals("farmland"))
+			worldIn.setBlockState(blockPos, Blocks.FARMLAND.getDefaultState().withProperty(MOISTURE, 7));
 	}
 }
