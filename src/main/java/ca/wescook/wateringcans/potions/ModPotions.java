845c7fcb85e6f81dc9563ebb4d02d67fb3f71ec7
Wes Cook <im@wescook.ca>
2017-06-21 10:14:59 -0700

    Fully registering potions to prevent ghost effects (fixes #3)

--- src/main/java/ca/wescook/wateringcans/potions/ModPotions.java
+++ src/main/java/ca/wescook/wateringcans/potions/ModPotions.java
@@ -1,6 +1,7 @@
 package ca.wescook.wateringcans.potions;
 
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModPotions {
 	public static PotionInvisible usingWateringCan;
@@ -11,14 +12,20 @@ public class ModPotions {
 		// Using watering can
 		usingWateringCan = new PotionInvisible(false, 0);
 		usingWateringCan.setPotionName("usingWateringCan");
+		usingWateringCan.setRegistryName("using_watering_can");
+		GameRegistry.register(usingWateringCan);
 
 		// Slow player
 		slowPlayer = new PotionInvisible(true, 0);
-		slowPlayer.setPotionName("invSlow");
+		slowPlayer.setPotionName("slowPlayer");
+		slowPlayer.setRegistryName("slow_player");
 		slowPlayer.registerPotionAttributeModifier(SharedMonsterAttributes.MOVEMENT_SPEED, "2a050f8c-07be-4e06-9d39-b6d299e0505f", -0.15D, 2);
+		GameRegistry.register(slowPlayer);
 
 		// Inhibit FOV changes
 		inhibitFOV = new PotionInvisible(false, 0);
 		inhibitFOV.setPotionName("inhibitFOV");
+		inhibitFOV.setRegistryName("inhibit_fov");
+		GameRegistry.register(inhibitFOV);
 	}
 }
